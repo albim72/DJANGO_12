63671de7e3fd61da7e476b78feda448646d135ec
albim72 <marcin.albiniak72@gmail.com>
2023-12-04 15:17:49 +0100

    Add files via upload

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from todo.models import ToDoItem
+from datetime import date
 
 class AllToDos(ListView):
     model = ToDoItem
     template_name = "todo/index.html"
+
+class TodayToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/today.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date=date.today())
+
 
 
